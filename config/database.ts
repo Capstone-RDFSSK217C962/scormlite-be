ef36f99f0c64eb850ed66e85e22dc1cc37cca116
victoriatrinita <victoriapardede@gmail.com>
2022-03-28 17:56:11 +0700

    comment mysql

--- config/database.ts
+++ config/database.ts
@@ -53,21 +53,21 @@ const databaseConfig: DatabaseConfig = {
     |
     */
 
-    mysql: {
-      client: 'mysql',
-      connection: {
-        host: Env.get('MYSQL_HOST'),
-        port: Env.get('MYSQL_PORT'),
-        user: Env.get('MYSQL_USER'),
-        password: Env.get('MYSQL_PASSWORD', ''),
-        database: Env.get('MYSQL_DB_NAME'),
-      },
-      migrations: {
-        naturalSort: true,
-      },
-      healthCheck: false,
-      debug: false,
-    },
+    // mysql: {
+    //   client: 'mysql',
+    //   connection: {
+    //     host: Env.get('MYSQL_HOST'),
+    //     port: Env.get('MYSQL_PORT'),
+    //     user: Env.get('MYSQL_USER'),
+    //     password: Env.get('MYSQL_PASSWORD', ''),
+    //     database: Env.get('MYSQL_DB_NAME'),
+    //   },
+    //   migrations: {
+    //     naturalSort: true,
+    //   },
+    //   healthCheck: false,
+    //   debug: false,
+    // },
   },
 }
 
